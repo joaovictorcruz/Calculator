dbc0dd494e864fbc5e21fcdd3aeef77cad1b135a
João Victor <elietejvc@hotmail.com>
2025-03-13 11:07:33 -0300

    iniciando calculadora

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro número");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+
+        }
+    }
+}
