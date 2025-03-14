6fc814e6d0eca0683e43654988614aa340d2bc86
João Victor <elietejvc@hotmail.com>
2025-03-14 10:48:23 -0300

    adicionando metodo de subtraçao

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Subtracao();
+        }
+
+        static void Soma()
+        {
             Console.Clear();
             Console.WriteLine("Digite o primeiro número");
             float v1 = float.Parse(Console.ReadLine());
@@ -17,9 +22,25 @@ namespace Calculator
             Console.WriteLine("");
 
             float resultado = v1 + v2;
-            Console.WriteLine("O resultado é: " + resultado);
-            Console.WriteLine($"O resultado é: {resultado}");
-            Console.WriteLine($"O resultado é: {v1 + v2}");
+            Console.WriteLine($"O resultado da some é: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro número");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Digite o segundo número");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey();
         }
     }
 }
