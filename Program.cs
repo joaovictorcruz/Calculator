7218b4f58618a447a3799254c92c831752571fa5
João Victor <elietejvc@hotmail.com>
2025-03-14 10:36:32 -0300

    concatenação e interpolação de string

--- Program.cs
+++ Program.cs
@@ -10,8 +10,16 @@ namespace Calculator
             Console.WriteLine("Digite o primeiro número");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.Clear();
+            Console.WriteLine("Digite o segundo número");
+            float v2 = float.Parse(Console.ReadLine());
 
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado é: " + resultado);
+            Console.WriteLine($"O resultado é: {resultado}");
+            Console.WriteLine($"O resultado é: {v1 + v2}");
         }
     }
 }
